150790ddabc162fc30480ea0ce96c8e452e48452
Pavel Romanov <romanovp8913@gmail.com>
2025-05-02 20:30:23 +0700

    feat: ship cjs version of all packages in the build

--- packages/test-core/eslint.config.mjs
+++ packages/test-core/eslint.config.mjs
@@ -1,11 +1,9 @@
 import baseConfig from '../../eslint.config.mjs';
 import coreConfig from '../eslint-config-core/src/lib/config-core.mjs';
-import angularConfig from '../eslint-config-angular/src/lib/config-angular.mjs';
 
 export default [
   ...baseConfig,
   ...coreConfig,
-  ...angularConfig,
   {
     files: ['**/*.json'],
     rules: {
