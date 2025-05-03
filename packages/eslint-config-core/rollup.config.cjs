e7e2265747fa506838c43da39a845893c6f09e1a
Pavel Romanov <romanovp8913@gmail.com>
2025-05-03 16:18:31 +0700

    fix: declare packages dependencies manually

--- packages/eslint-config-core/rollup.config.cjs
+++ packages/eslint-config-core/rollup.config.cjs
@@ -1,12 +1,10 @@
 const { withNx } = require('@nx/rollup/with-nx');
 
-module.exports = withNx(
-  {
-    main: './src/index.ts',
-    outputPath: './dist',
-    tsConfig: './tsconfig.lib.json',
-    compiler: 'tsc',
-    external: "all",
-    format: ['esm', 'cjs'],
-  },
-);
+module.exports = withNx({
+  main: './src/index.ts',
+  outputPath: './dist',
+  tsConfig: './tsconfig.lib.json',
+  compiler: 'tsc',
+  external: 'all',
+  format: ['esm', 'cjs'],
+});
