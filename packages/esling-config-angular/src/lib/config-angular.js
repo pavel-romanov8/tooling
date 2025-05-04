b7c9444f096b4f927c25d64ab29da1f5d6880074
Pavel Romanov <romanovp8913@gmail.com>
2025-05-04 19:35:48 +0700

    fix(angular): provide parser options for angular eslint

--- packages/esling-config-angular/src/lib/config-angular.js
+++ packages/esling-config-angular/src/lib/config-angular.js
@@ -5,12 +5,24 @@ import angularEslint from 'angular-eslint';
 export default tseslint.config(
   {
     files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(),
+      },
+    },
     extends: [...angularEslint.configs.tsRecommended],
   },
 
   {
     files: ['**/*.component.ts', '**/*.component.html'],
     processor: angularEslint.processInlineTemplates,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(),
+      },
+    },
     extends: [
       ...angularEslint.configs.templateRecommended,
       ...angularEslint.configs.templateAccessibility,
